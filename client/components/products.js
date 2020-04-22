c0f6c87af586b40e7ee427be83a80f9db487b146
Justin Davidson <justindavidson45@gmail.com>
2020-04-22 14:05:05 -0500

    all products view working

--- client/components/products.js
+++ client/components/products.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {getAllProducts} from '../store/products'
 
-export class Products extends React.Component {
+class Products extends React.Component {
+  componentDidMount() {
+    this.props.getProducts()
+    console.log(this.props)
+  }
   render() {
     return (
       <div>
-        <div>{'in here will be the iteration of <Product /> component'}</div>
+        <div>
+          {this.props.products.map(product => {
+            return (
+              <div key={product.id}>
+                <img src={product.image} />
+                <h3>Title: </h3>
+                <p>{product.title}</p>
+                <h3>Description: </h3>
+                <p>{product.description}</p>
+                <h3>Price: </h3>
+                <p>{product.price}</p>
+                <h3>Quantity: </h3>
+                <p>{product.quantity}</p>
+                <h3>Type: </h3>
+                <p>{product.category ? product.category.name : 'null'}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
 }
+
+const mapState = state => {
+  return {
+    products: state.products
+  }
+}
+const mapDispatch = dispatch => {
+  return {
+    getProducts: () => dispatch(getAllProducts())
+  }
+}
+export default connect(mapState, mapDispatch)(Products)
--- server/api/products.js
+++ server/api/products.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const Product = require('../db/models/product')
+const Category = require('../db/models/category')
+
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Product.findAll()
+    const products = await Product.findAll({include: [Category]})
     res.json(products)
   } catch (err) {
     next(err)
