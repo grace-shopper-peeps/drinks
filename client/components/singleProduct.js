6972f9e1fb5c137b5933f97a9d631b8758307a06
Claire Muscat <clairemuscat94@gmail.com>
2020-04-22 14:12:08 -0500

    trying to add routes

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import {fetchProductThunk} from '../store.product'
+import {fetchProductThunk} from '../store/product'
 import {connect} from 'react-redux'
 
 export class Product extends React.Component {
+  componentDidMount() {
+    this.props.getProduct(this.props.match.params.productId)
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>
         <h3>PRODUCT NAME HERE</h3>
