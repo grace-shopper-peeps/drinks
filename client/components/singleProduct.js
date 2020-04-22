139c3e0bf075b6d0a8f99a04ff865794bdb7f7e1
Claire Muscat <clairemuscat94@gmail.com>
2020-04-22 11:47:50 -0500

    testing commit of single product

---
+++ client/components/singleProduct.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {fetchProductThunk} from '../store.product'
+import {connect} from 'react-redux'
+
+export class Product extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>PRODUCT NAME HERE</h3>
+        <img src="IMAGE HERE" />
+        <div>PRODUCT PRICE HERE</div>
+        <p>PRODUCT DESCRIPTION HERE</p>
+      </div>
+    )
+  }
+}
+
+export const mapStateToProps = state => ({
+  product: state.product
+})
+
+export const mapDispatchToProps = dispatch => {
+  return {
+    getProduct: id => dispatch(fetchProductThunk(id))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product)
