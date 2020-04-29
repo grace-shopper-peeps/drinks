274c6587af8b6ba8c0caf9acddbe8ccb284b84d0
Symone Varnado <varnado@bc.edu>
2020-04-29 03:29:33 -0400

    added banner to logged in user

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -11,14 +11,25 @@ export const UserHome = props => {
 
   return (
     <div>
-      {!isAdmin ? (
-        <h3 className="animated fadeIn">Welcome, {email}</h3>
-      ) : (
-        <div>
-          <h3>Welcome, admin {email}</h3>
-          <Link to="/users">All Users</Link>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">20% off all Bourbons!</h1>
+          <p class="lead">
+            We're so excited for you to join us! Take advantage of all our
+            liquors from across the globe
+          </p>
         </div>
-      )}
+      </div>
+      <div>
+        {!isAdmin ? (
+          <h3 className="animated fadeIn">Welcome, {email}</h3>
+        ) : (
+          <div>
+            <h3>Welcome, admin {email}</h3>
+            <Link to="/users">All Users</Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
